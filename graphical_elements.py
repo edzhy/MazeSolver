3c81e60d84394186306707ebaa10a666aa8e3d07
Edgars <the1edgars@gmail.com>
2025-05-01 19:16:38 +0300

    drawing cells on the grid

--- graphical_elements.py
+++ graphical_elements.py
@@ -23,7 +23,10 @@ class Window():
         self.__win_running = False
 
     def draw_line(self, line, fill_color):
-        line.draw(self.canva,fill_color)
+        line.draw(self.canva, fill_color)
+
+    def draw_cell(self, cell, fill_color):
+       cell.draw(fill_color)
 
     
 class Point():
@@ -40,3 +43,40 @@ class Line():
         #fill_color will be a string, ex.: "red"
         canvas.create_line(self.l_start.x,self.l_start.y,self.l_end.x,self.l_end.y, fill=fill_color, width=2)
 
+class Cell():
+    def __init__(self, x1, y1, x2, y2, win, lw=True, rw=True, tw=True, bw=True):
+        self.has_left_wall = lw
+        self.has_right_wall = rw
+        self.has_top_wall = tw
+        self.has_bottom_wall = bw
+        #top-left corner coordinates
+        self._x1 = x1
+        self._y1 = y1
+        #bottom-right corner coordinates
+        self._x2 = x2
+        self._y2 = y2
+        self._win = win
+    
+    def draw(self, fill_color):
+        p1, p2 = Point(self._x1, self._y1), Point(self._x2, self._y2)
+        #opposite corner creation math
+        #top-right p3 corner will have x as x2, y as y1,
+        #bottom-left p4 corner x as x1, y as y2
+        p3, p4 = Point(self._x2, self._y1), Point(self._x1, self._y2)
+        #
+        #   p1        p3
+        #
+        #       cell
+        #
+        #   p4        p2
+        #
+        #topwall rightwall bottomwall leftwall creation
+        tw, rw, bw, lw = Line(p1,p3),Line(p3,p2),Line(p2,p4),Line(p1,p4)
+        if self.has_top_wall:
+            self._win.draw_line(tw,fill_color)
+        if self.has_right_wall:
+            self._win.draw_line(rw,fill_color)
+        if self.has_bottom_wall:
+            self._win.draw_line(bw,fill_color)
+        if self.has_left_wall:
+            self._win.draw_line(lw,fill_color)
--- main.py
+++ main.py
@@ -6,10 +6,13 @@ def main():
     #drawing some lines:
     point1, point2 = Point(700,600), Point(600,500)
     point3, point4 = Point(400,300), Point(500,400)
-    line1, line2 = Line(point1, point2), Line(point3, point4)
-    win.draw_line(line1)
-    win.draw_line(line2, "black")
-
+    #line1, line2 = Line(point1, point2), Line(point3, point4)
+    #win.draw_line(line1, "red")
+    #win.draw_line(line2, "black")
+    cell1 = Cell(500,500,400,400,win,lw=False)
+    win.draw_cell(cell1, "red")
+    cell2 = Cell(490,490,410,410,win,rw=False)
+    win.draw_cell(cell2, "black")
     win.wait_for_close()
 
 main()
